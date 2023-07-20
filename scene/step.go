69cb52febadb73d6c10a97d2dea7b9389a8a3c77
liminin <liminin@list.ru>
2023-07-20 11:26:49 +0300

    .

--- scene/step.go
+++ scene/step.go
@@ -1,6 +1,16 @@
 package scene
 
+import "github.com/liminin/goscene/state"
+
 type Play[T any] interface {
+	FirstTime() bool
+	State() *state.State
+	SetData(data T)
+	Execute() error
+	Next() error
+	Back() error
+	Go(i int) error
+	Exit() error
 }
 
 type Handler[T any] func(play Play[T], data T) error
